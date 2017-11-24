965f633c6b007f52f208f7463812a9edfe0127a3
fabiopolimeni <fabio.polimeni@outlook.com>
2017-11-24 20:58:40 +0100

    Remove parenthesis

--- components/facebook/skills/postbacks.js
+++ components/facebook/skills/postbacks.js
@@ -46,7 +46,7 @@ module.exports = function (controller, middleware, database) {
           controller.trigger('audio_transcript', [bot, transcript_message]);
       }
 
-    });
+    }
   });
   
 }
